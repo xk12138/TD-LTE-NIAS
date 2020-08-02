1780f73d88e47a08f68625eded74745acea4d544
xk12138 <38340557+xk12138@users.noreply.github.com>
2020-08-02 21:12:32 +0800

    修改PRB的数据库设计

---
+++ back/src/main/java/com/example/back/repository/PRBRepository.java
@@ -0,0 +1,14 @@
+package com.example.back.repository;
+
+import com.example.back.model.PRB;
+import com.example.back.model.PRBKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PRBRepository extends JpaRepository<PRB, PRBKey> {
+
+    @Query(value = "create table if not exists tbPRBnew (select " +
+            " 起始时间, 60 as 周期， 网元名称")
+    void createIfNotExists();
+
+}
