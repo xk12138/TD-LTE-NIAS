24e0114c25eb210304dd6ccb33752086b759efbc
huanke <1917375043@qq.com>
2020-08-02 22:44:36 +0800

    将前端导航菜单改为标签页

--- front/src/components/data_control.vue
+++ front/src/components/data_control.vue
@@ -1,21 +1,8 @@
 <template>
-  <el-menu
-    :default-active="activeIndex"
-    class="el-menu-demo"
-    mode="horizontal"
-    @select="handleSelect"
-    background-color="#545c64"
-    text-color="#fff"
-    active-text-color="#ffd04b">
-    <el-submenu index="1">
-      <template slot="title">数据导入</template>
-      <el-menu-item index="1-1">导入tbCell</el-menu-item>
-      <el-menu-item index="1-2">导入tbKPI</el-menu-item>
-      <el-menu-item index="1-3">导入tbPRB</el-menu-item>
-      <el-menu-item index="1-4">导入tbMROData</el-menu-item>
-    </el-submenu>
-    <el-menu-item index="2" disabled>数据下载</el-menu-item>
-  </el-menu>
+  <el-tabs v-model="activeName" @tab-click="handleClick">
+    <el-tab-pane label="上传数据" name="first">上传数据</el-tab-pane>
+    <el-tab-pane label="下载数据" name="second">下载数据</el-tab-pane>
+  </el-tabs>
 </template>
 
 <script>
@@ -23,12 +10,12 @@
       name: "data_control",
       data(){
         return{
-          activeIndex:'1'
+          activeName: 'second'
         };
       },
       methods: {
-        handleSelect(key, keyPath) {
-          console.log(key, keyPath);
+        handleClick(tab, event) {
+          console.log(tab, event);
         }
       }
     }
--- front/src/components/info_search.vue
+++ front/src/components/info_search.vue
@@ -1,10 +1,25 @@
 <template>
-
+  <el-tabs v-model="activeName" @tab-click="handleClick">
+    <el-tab-pane label="小区配置信息查询" name="first">小区配置信息查询</el-tab-pane>
+    <el-tab-pane label="基站eNodeB信息查询" name="second">基站eNodeB信息查询</el-tab-pane>
+    <el-tab-pane label="KPI指标信息查询" name="third">KPI指标信息查询</el-tab-pane>
+    <el-tab-pane label="PRB信息统计与查询" name="forth">PRB信息统计与查询</el-tab-pane>
+  </el-tabs>
 </template>
 
 <script>
     export default {
         name: "info_search"
+      data(){
+        return{
+          activeName: 'second'
+        };
+      },
+      methods: {
+        handleClick(tab, event) {
+          console.log(tab, event);
+        }
+      }
     }
 </script>
 
