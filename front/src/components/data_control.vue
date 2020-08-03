51fdef46edafda42beca4539a8f9c7f6ea42b5e9
huanke <1917375043@qq.com>
2020-08-03 15:07:24 +0800

    测试文件上传接口失败，修改了文件上传组件

--- front/src/components/data_control.vue
+++ front/src/components/data_control.vue
@@ -2,18 +2,25 @@
   <el-tabs v-model="activeName" @tab-click="handleClick">
     <el-tab-pane label="上传数据" name="first">上传数据
       <el-upload
-        style="display:inline-block"
-        :limit="5"
-        class="upload-demo"
-        ref="upload"
-        :action="uploadUrl"
-        :file-list="fileList"
-        :http-request="uploadSectionFile"
+        ref="uploadExcel"
+        action="http://127.0.0.1:8080/kpi/import"
+        :limit="limitNum"
         :auto-upload="false"
-        :before-remove="handleRemove">
-        <el-button slot="trigger" size="small" type="primary" plain>选取文件</el-button>
-        <el-button style="margin-left: 10px;" size="small" icon="el-icon-upload2" type="success" @click="submitUpload">导入</el-button>
+        accept=".xlsx,.csv"
+        :before-upload="beforeUploadFile"
+        :on-change="fileChange"
+        :on-exceed="exceedFile"
+        :on-success="handleSuccess"
+        :on-error="handleError"
+        :file-list="fileList"
+
+      >
+        <el-button size="small" plain>选择文件</el-button>
+        <div slot="tip" class="el-upload__tip">
+          只能上传xlsx/csv文件，且文件大小不超过50M
+        </div>
       </el-upload>
+      <el-button type="primary" @click="uploadClick">确 定</el-button>
     </el-tab-pane>
 <!--    limit是限制最多可上传文件的个数；action的地址是将文件传给后台的接口地址； -->
 <!--    fileList是选择的文件的全部信息，在事件中作为参数传进去，可以查看其内容；-->
@@ -26,7 +33,9 @@
       name: "data_control",
       data(){
         return{
-          activeName: 'first'
+          activeName: 'first',
+          limitNum: 5,//最多上传五个文件
+          fileList: []
         };
       },
       methods: {
@@ -34,24 +43,58 @@
           console.log(tab, event);
         },
 
-        uploadSectionFile(param){
-          var fileObj = param.file;
-          // FormData 对象
-          var form = new FormData();
-          // 文件对象
-          form.append("file", fileObj);
-          form.append("userId", this.userId);
-          form.append("userName", this.userName);
-          axios({
-            method: 'post',
-            url: '',//todo:接口地址
-            headers: {
-              //'Content-Type': 'multipart/form-data'
-            },
-            data:form
-          }).then(res => {
-            //todo：返回后
+        // 上传文件之前的钩子, 参数为上传的文件,若返回 false 或者返回 Promise 且被 reject，则停止上传
+        beforeUploadFile(file) {
+           console.log(file)
+          const extension = file.name.substring(file.name.lastIndexOf('.') + 1)//文件后缀
+          const size = file.size / 1024 / 1024
+          if (extension !== 'xlsx') {
+            this.$notify.warning({//文件类型控制
+              title: '警告',
+              message: `只能上传Excel或csv的文件`
+            })
+          }
+          if (size > 50) {
+            this.$notify.warning({//文件大小控制
+              title: '警告',
+              message: `文件大小不得超过50M`
+            })
+          }
+        },
+        // 文件状态改变时的钩子
+        fileChange(file, fileList) {
+           // console.log(file)
+           // console.log(fileList)
+        },
+        // 文件超出个数限制时的钩子
+        exceedFile(files, fileList) {
+          this.$notify.warning({
+            title: '警告',
+            message: `只能选择 ${
+              this.limitNum
+            } 个文件，当前共选择了 ${files.length + fileList.length} 个`
           })
+        },
+        // 文件上传成功时的钩子
+        handleSuccess(res, file, fileList) {
+          console.log(res)
+          // this.formData.url = res.data  //服务器返回的文件地址
+          this.$message({
+            message: '文件上传成功',
+            type: 'success'
+          })
+          this.$refs.uploadExcel.clearFiles()// 清除上次上传记录
+        },
+        // 文件上传失败时的钩子
+        handleError(err, file, fileList) {
+          this.$message({
+            message:'文件上传失败',
+            type:'error'
+          })
+        },
+
+        uploadClick() {//上传文件
+          this.$refs.uploadExcel.submit()
         },
 
       }
