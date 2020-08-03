c13ca8684628a1ae0d4f7fb198b5afbebec4d43e
xk12138 <38340557+xk12138@users.noreply.github.com>
2020-08-03 21:35:35 +0800

    KPI导入

--- back/src/main/java/com/example/back/service/impl/KPIServiceImpl.java
+++ back/src/main/java/com/example/back/service/impl/KPIServiceImpl.java
@@ -5,7 +5,9 @@ import com.example.back.repository.KPIRepository;
 import com.example.back.service.KPIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,8 +17,13 @@ public class KPIServiceImpl implements KPIService {
     KPIRepository kpiRepository;
 
     @Override
+    @Transactional
     public void importKPI(List<KPI> kpis) {
-        kpiRepository.importKPI(kpis);
+        int page = kpis.size() / 50;
+        for (int i = 0; i < page; i++) {
+            List<KPI> temp = kpis.subList(i * 50, i * 50 + 50);
+            kpiRepository.saveAll(temp);
+        }
     }
 
 }
