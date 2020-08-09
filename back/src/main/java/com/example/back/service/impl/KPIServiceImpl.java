aeee2c80f95cf89953fac6458367bfd690883114
xk12138 <38340557+xk12138@users.noreply.github.com>
2020-08-09 23:12:37 +0800

    KPI

--- back/src/main/java/com/example/back/service/impl/KPIServiceImpl.java
+++ back/src/main/java/com/example/back/service/impl/KPIServiceImpl.java
@@ -11,6 +11,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -44,42 +46,30 @@ public class KPIServiceImpl implements KPIService {
                     "RRC连接建立完成次数=VALUES(RRC连接建立完成次数),RRC连接请求次数_包括重发=VALUES(RRC连接请求次数_包括重发)," +
                     "RRC建立成功率qf=VALUES(RRC建立成功率qf),E_RAB建立成功总次数=VALUES(E_RAB建立成功总次数)," +
                     "E_RAB建立尝试总次数=VALUES(E_RAB建立尝试总次数),E_RAB建立成功率=VALUES(E_RAB建立成功率)," +
-                    "eNodeB触发的E_RAB异常释放总次数=VALUES(eNodeB触发的E_RAB异常释放总次数)," +
+                    "e_nodeB触发的E_RAB异常释放总次数=VALUES(e_nodeB触发的E_RAB异常释放总次数)," +
                     "小区切换出E_RAB异常释放总次数=VALUES(小区切换出E_RAB异常释放总次数)," +
                     "E_RAB掉线率_新=VALUES(E_RAB掉线率_新),无线接通率ay=VALUES(无线接通率ay)," +
-                    "eNodeB发起的S1_RESET导致的UE_Context释放次数=VALUES(eNodeB发起的S1_RESET导致的UE_Context释放次数)," +
+                    "e_nodeB发起的S1_RESET导致的UE_Context释放次数=VALUES(e_nodeB发起的S1_RESET导致的UE_Context释放次数)," +
                     "UE_Context异常释放次数=VALUES(UE_Context异常释放次数),UE_Context建立成功总次数=VALUES(UE_Context建立成功总次数)," +
                     "无线掉线率=VALUES(无线掉线率)," +
-                    "eNodeB内异频切换出成功次数=VALUES(eNodeB内异频切换出成功次数)," +
-                    "eNodeB内异频切换出尝试次数=VALUES(eNodeB内异频切换出尝试次数)," +
-                    "eNodeB内同频切换出成功次数=VALUES(eNodeB内同频切换出成功次数)," +
-                    "eNodeB内同频切换出尝试次数=VALUES(eNodeB内同频切换出尝试次数)," +
-                    "eNodeB间异频切换出成功次数=VALUES(eNodeB间异频切换出成功次数)," +
-                    "eNodeB间异频切换出尝试次数=VALUES(eNodeB间异频切换出尝试次数)," +
+                    "e_nodeB内异频切换出成功次数=VALUES(e_nodeB内异频切换出成功次数)," +
+                    "e_nodeB内异频切换出尝试次数=VALUES(e_nodeB内异频切换出尝试次数)," +
+                    "e_nodeB内同频切换出成功次数=VALUES(e_nodeB内同频切换出成功次数)," +
+                    "e_nodeB内同频切换出尝试次数=VALUES(e_nodeB内同频切换出尝试次数)," +
+                    "e_nodeB间异频切换出成功次数=VALUES(e_nodeB间异频切换出成功次数)," +
+                    "e_nodeB间异频切换出尝试次数=VALUES(e_nodeB间异频切换出尝试次数)," +
+                    "e_nodeB间同频切换出成功次数=VALUES(e_nodeB间同频切换出成功次数)," +
+                    "e_nodeB间同频切换出尝试次数=VALUES(e_nodeB间同频切换出尝试次数)," +
                     "eNB内切换成功率=VALUES(eNB内切换成功率),eNB间切换成功率=VALUES(eNB间切换成功率)," +
                     "同频切换成功率zsp=VALUES(同频切换成功率zsp),异频切换成功率zsp=VALUES(异频切换成功率zsp)," +
                     "切换成功率=VALUES(切换成功率),小区PDCP层所接收到的上行数据的总吞吐量=VALUES(小区PDCP层所接收到的上行数据的总吞吐量)," +
                     "小区PDCP层所发送的下行数据的总吞吐量=VALUES(小区PDCP层所发送的下行数据的总吞吐量)," +
                     "RRC重建请求次数=VALUES(RRC重建请求次数),RRC连接重建比率=VALUES(RRC连接重建比率)," +
-                    "通过重建回源小区的eNodeB间同频切换出执行成功次数=VALUES(通过重建回源小区的eNodeB间同频切换出执行成功次数)," +
-                    "通过重建回源小区的eNodeB间异频切换出执行成功次数=VALUES(通过重建回源小区的eNodeB间异频切换出执行成功次数)," +
-                    "通过重建回源小区的eNodeB内同频切换出执行成功次数=VALUES(通过重建回源小区的eNodeB内同频切换出执行成功次数)," +
-                    "通过重建回源小区的eNodeB内异频切换出执行成功次数=VALUES(通过重建回源小区的eNodeB内异频切换出执行成功次数)," +
-                    "eNB内切换出成功次数=VALUES(eNB内切换出成功次数),eNB内切换出请求次数=VALUES(eNB内切换出请求次数)," +
-                    "e_nodeb内同频切换出尝试次数=VALUES(e_nodeb内同频切换出尝试次数)," +
-                    "e_nodeb内同频切换出成功次数=VALUES(e_nodeb内同频切换出成功次数)," +
-                    "e_nodeb内异频切换出尝试次数=VALUES(e_nodeb内异频切换出尝试次数)," +
-                    "e_nodeb内异频切换出成功次数=VALUES(e_nodeb内异频切换出成功次数)," +
-                    "e_nodeb发起的s1_reset导致的ue_context释放次数=VALUES(e_nodeb发起的s1_reset导致的ue_context释放次数)," +
-                    "e_nodeb触发的e_rab异常释放总次数=VALUES(e_nodeb触发的e_rab异常释放总次数)," +
-                    "e_nodeb间同频切换出尝试次数=VALUES(e_nodeb间同频切换出尝试次数)," +
-                    "e_nodeb间同频切换出成功次数=VALUES(e_nodeb间同频切换出成功次数)," +
-                    "e_nodeb间异频切换出尝试次数=VALUES(e_nodeb间异频切换出尝试次数)," +
-                    "e_nodeb间异频切换出成功次数=VALUES(e_nodeb间异频切换出成功次数)," +
-                    "通过重建回源小区的e_nodeb内同频切换出执行成功次数=VALUES(通过重建回源小区的e_nodeb内同频切换出执行成功次数)," +
-                    "通过重建回源小区的e_nodeb内异频切换出执行成功次数=VALUES(通过重建回源小区的e_nodeb内异频切换出执行成功次数)," +
-                    "通过重建回源小区的e_nodeb间同频切换出执行成功次数=VALUES(通过重建回源小区的e_nodeb间同频切换出执行成功次数)," +
-                    "通过重建回源小区的e_nodeb间异频切换出执行成功次数=VALUES(通过重建回源小区的e_nodeb间异频切换出执行成功次数);");
+                    "通过重建回源小区的e_nodeB间同频切换出执行成功次数=VALUES(通过重建回源小区的e_nodeB间同频切换出执行成功次数)," +
+                    "通过重建回源小区的e_nodeB间异频切换出执行成功次数=VALUES(通过重建回源小区的e_nodeB间异频切换出执行成功次数)," +
+                    "通过重建回源小区的e_nodeB内同频切换出执行成功次数=VALUES(通过重建回源小区的e_nodeB内同频切换出执行成功次数)," +
+                    "通过重建回源小区的e_nodeB内异频切换出执行成功次数=VALUES(通过重建回源小区的e_nodeB内异频切换出执行成功次数)," +
+                    "eNB内切换出成功次数=VALUES(eNB内切换出成功次数),eNB内切换出请求次数=VALUES(eNB内切换出请求次数)");
             System.out.println(stringBuilder.toString());
             e.createNativeQuery(stringBuilder.toString()).executeUpdate();
         }
@@ -96,8 +86,9 @@ public class KPIServiceImpl implements KPIService {
 
     @Override
     public List<Map<String, Object>> search(String keyword, String name, Date startTime, Date endTime) {
-        String sql = "select " + keyword + " from tbKPI where 网元名称=:网元名称 and timestampdiff(day, :start_time, 起始时间)>=0 and timestampdiff(day, :end_time, 起始时间)<=0";
-        return e.createNativeQuery(sql).setParameter("网元名称", name).setParameter("start_time", startTime).setParameter("end_time", endTime).getResultList();
+        DateFormat dateFormat = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
+        String sql = "select " + keyword + " from tbKPI where 网元名称=\'" + name + "\' and timestampdiff(day, \'" + dateFormat.format(startTime) + "\', 起始时间)>=0 and timestampdiff(day, \'" + dateFormat.format(endTime) + "\', 起始时间)<=0";
+        return e.createNativeQuery(sql).getResultList();
     }
 
 }
