358354aa13f09c4637671a8a371e17f727c20ee3
WuyueWuyue519 <1125322104@qq.com>
2020-08-09 23:44:46 +0800

    tbCell表的两个查询语句

--- back/src/main/java/com/example/back/service/CellService.java
+++ back/src/main/java/com/example/back/service/CellService.java
@@ -2,12 +2,19 @@ package com.example.back.service;
 
 import com.example.back.model.Cell;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 public interface CellService {
 
     void importCell(List<Cell> Cells);
 
     void exportCELL(String filePath);
+
+    List<Map<String, Object>> search_by_sector(String keyword);
+
+    List<Map<String, Object>> search_by_enodeB(String keyword);
+
 
 }
